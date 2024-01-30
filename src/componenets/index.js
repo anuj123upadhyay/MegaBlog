85e265774715dfc3f4c4d71bb51da9079ad9900e
anuj upadhyay <anuju760@gmail.com>
2024-01-30 19:58:02 +0530

    rectify a error during deployment

--- src/componenets/index.js
+++ src/componenets/index.js
@@ -1,6 +1,6 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./container/container";
+import Container from "./container/Container";
 import Logo from './Logo'
 import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button"
