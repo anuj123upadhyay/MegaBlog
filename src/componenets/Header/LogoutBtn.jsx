e242ac0c8b3a0bad5e2680aa28a411bf24d0bdc4
Ayush <asharma7588@gmail.com>
2024-10-04 21:55:47 +0530

    Logout confirmation modal

--- src/componenets/Header/LogoutBtn.jsx
+++ src/componenets/Header/LogoutBtn.jsx
@@ -1,23 +1,58 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import authService from '../../appwrite/auth'
-import { logout  } from "../../store/authSlice"
-
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "../../appwrite/auth";
+import { logout } from "../../store/authSlice";
 
 function LogoutBtn() {
+  const [showModal, setShowModal] = useState(false);
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
-    const logoutHandler = ()=>{
-        authService.logout().then(()=>{
-            dispatch(logout())
-        })
-    }
+  const logoutHandler = () => {
+    authService.logout().then(() => {
+      dispatch(logout());
+    });
+    setShowModal(false); // Close modal after logout
+  };
 
   return (
-   <button className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full ' 
-   onClick={logoutHandler}
-   >Logout</button>
-  )
+    <>
+      {/* Logout Button */}
+      <button
+        className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+        onClick={() => setShowModal(true)} // Show modal when button is clicked
+      >
+        Logout
+      </button>
+
+      {/* Confirmation Modal */}
+      {showModal && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+          <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full">
+            <h2 className="text-lg font-bold mb-4 text-center">
+              Are you sure you want to log out?
+            </h2>
+            <div className="flex justify-around">
+              {/* Cancel Button */}
+              <button
+                className="px-4 py-2 bg-gray-200 text-gray-800 rounded hover:bg-gray-300"
+                onClick={() => setShowModal(false)}
+              >
+                Cancel
+              </button>
+
+              {/* Confirm Logout Button */}
+              <button
+                className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+                onClick={logoutHandler}
+              >
+                Confirm Logout
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
-export default LogoutBtn
+export default LogoutBtn;
