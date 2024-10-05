5dd9d6afd0699ff12c1e184e786d50193a6ac162
Lakshay <lakshaypahuja777@gmail.com>
2024-10-05 19:26:00 +0530

    toggle text color button added

---
+++ src/componenets/Header/Header.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { Container, Logo, LogoutBtn } from '../index'
+
+function Header() {
+  const authStatus = useSelector((state) => state.auth.status)
+  const navigate = useNavigate()
+
+  /* Toggle Text Color */
+  const toggleTextColor = () => {
+    const rootElement = document.getElementById('root');
+
+    // rootElement.classList.toggle('black-color');
+    rootElement.classList.toggle('white-color');
+
+    const footerLinks = document.getElementsByClassName('toggle');
+    Array.from(footerLinks).forEach(link => {
+      link.classList.toggle('text-gray-900');
+      link.classList.toggle('text-alice-blue-900');
+    });
+
+    // const rootElement1 = document.getElementsByClassName('min-h-screen');
+    // rootElement1.classList.toggle('black-bg');
+  }
+/* Toggle Text Color */
+
+  const navItems = [
+    {
+      name: 'Home',
+      slug: "/",
+      active: true
+    }, 
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+    },
+  ]
+
+  return (
+    <header className='py-3 shadow bg-gray-500'>
+      <Container>
+        <nav className='flex'>
+          <div className='mr-4'>
+            <Link to='/'>
+              <Logo width='70px' />
+            </Link>
+          </div>
+          <ul className='flex ml-auto'>
+            <li>
+              <button
+                onClick={toggleTextColor}
+                className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>
+                Toggle Text Color
+              </button>
+            </li>
+            {navItems.map((item) => 
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    onClick={() => navigate(item.slug)}
+                    className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                  >
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Container>
+    </header>
+  )
+}
+
+export default Header;
