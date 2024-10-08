38e22bd3bf3cfd196230032557e267c425908a7c
Janvi <127668005+inkerton@users.noreply.github.com>
2024-10-08 06:13:23 +0000

    pricing button highlighted

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -116,10 +116,11 @@ function Pricing() {
                             </ul>
 
                             <a
-                                href="#"
-                                className="mt-8 block rounded-full border border-indigo-600 bg-indigo-600 px-12 py-3 text-center text-sm font-medium text-white hover:bg-indigo-700 hover:ring-1 hover:ring-indigo-700 focus:outline-none focus:ring active:text-indigo-500"
+                                href="/"
+                                className=" bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 mt-8 block rounded-full border border-indigo-600 bg-indigo-600 px-12 py-3 text-center text-sm font-medium text-white hover:bg-indigo-700 hover:ring-1 hover:ring-indigo-700 focus:outline-none focus:ring active:text-indigo-500"
                             >
-                                Get Started
+                                {/* Get Started */}
+                                Coming Soon
                             </a>
                         </div>
 
@@ -131,7 +132,7 @@ function Pricing() {
                                 </h2>
 
                                 <p className="mt-2 sm:mt-4">
-                                    <strong className="text-3xl font-bold text-gray-900 sm:text-4xl"> 20$ </strong>
+                                    <strong className="text-3xl font-bold text-gray-900 sm:text-4xl"> 0$ </strong>
 
                                     <span className="text-sm font-medium text-gray-700">/month</span>
                                 </p>
@@ -150,7 +151,7 @@ function Pricing() {
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
                                     </svg>
 
-                                    <span className="text-gray-700"> Unlock upto 10 blogs </span>
+                                    <span className="text-gray-700"> Unlock upto 100 blogs </span>
                                 </li>
 
                                 <li className="flex items-center gap-1">
@@ -165,7 +166,7 @@ function Pricing() {
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
                                     </svg>
 
-                                    <span className="text-gray-700"> 15 Blog Posts/month </span>
+                                    <span className="text-gray-700"> 15+ Blog Posts/month </span>
                                 </li>
 
                                 <li className="flex items-center gap-1">
@@ -200,7 +201,7 @@ function Pricing() {
                             </ul>
 
                             <a
-                                href="#"
+                                href="/signup"
                                 className="mt-8 block rounded-full border border-indigo-600 bg-white px-12 py-3 text-center text-sm font-medium text-indigo-600 hover:ring-1 hover:ring-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
                             >
                                 Get Started
