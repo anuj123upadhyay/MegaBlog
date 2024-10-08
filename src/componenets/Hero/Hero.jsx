33cfde0e5335929b9e52c0d8e299fbfa159ce880
anuj upadhyay <anuju760@gmail.com>
2024-10-08 19:14:00 +0530

    some changes in the hero

--- src/componenets/Hero/Hero.jsx
+++ src/componenets/Hero/Hero.jsx
@@ -12,23 +12,22 @@ function Hero() {
                         </h1>
 
                         <p className="mt-4 sm:text-xl/relaxed">
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus
-                            numquam ea!
+                           "Come at MegaBlog , where code and creativity blend, Unlocking new paths where innovations transcend."
                         </p>
 
                         <div className="mt-8 flex flex-wrap justify-center gap-4">
                             <a
                                 className="block w-full rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-black focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
-                                href="#"
+                                href="/add-posts"
                             >
-                                Get Started
+                                Write Post
                             </a>
 
                             <a
                                 className="block w-full rounded px-12 py-3 text-sm font-medium text-indigo-600 shadow hover:text-black-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
-                                href="#"
+                                href="/all-posts"
                             >
-                                Learn More
+                                Read Blogs
                             </a>
                         </div>
                     </div>
