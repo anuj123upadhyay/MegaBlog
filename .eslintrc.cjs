36b1862028b41b33b2b1436ac047d80cd53f6090
Rajashekar Reddy gunda <Enter your emailid here>
2024-10-12 18:31:41 +0530

    done

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,11 +10,8 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-Components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+  "plugins": ["react-refresh"],
+  "rules": {
+    "react-refresh/only-export-Components": "error"
+  }
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import authService from "./appwrite/auth"
 import { login, logout } from "./store/authSlice"
-import { Footer, Header } from './componenets'
-import { Outlet } from 'react-router-dom'
+import { Header } from './components/Header'
+import { Footer } from './components/Footer'
 import BackToTopButton from './components/ui/BackToTopButton'
+import NotFound from './components/NotFound'
 
 function App() {
   const [loading, setLoading] = useState(true)
@@ -21,18 +23,29 @@ function App() {
         }
       })
       .finally(() => setLoading(false))
-  }, [])
+  }, [dispatch])
 
   return !loading ? (
-    <div className='custom-theme min-h-screen flex flex-wrap content-between '>
-      <div className='w-full block'>
-        <BackToTopButton />
-        <Header />
-        <main>
-          <Outlet />
-        </main>
-        <Footer />
-      </div>
+    <div className='custom-theme min-h-screen flex flex-wrap content-between'>
+      <Routes>
+        {/* Define routes for your main app */}
+        <Route path="/" element={
+          <div className='w-full block'>
+            <BackToTopButton />
+            <Header />
+            <main>
+              <Outlet />
+            </main>
+            <Footer />
+          </div>
+        }>
+          {/* Add child routes here */}
+          {/* Example: <Route path="products" element={<Products />} /> */}
+        </Route>
+
+        {/* Catch-all route for 404 NotFound */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   ) : null
 }
