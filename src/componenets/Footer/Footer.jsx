60f895a392bde39a3033ff815e168a5028bd8f97
Anneshu Nag <132702983+NK-Works@users.noreply.github.com>
2024-11-10 11:13:54 +0530

    removed link bugs

--- src/componenets/Footer/Footer.jsx
+++ src/componenets/Footer/Footer.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Logo from "../Logo";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
