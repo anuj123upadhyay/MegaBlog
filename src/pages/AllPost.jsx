79b2933d764d732e45bedcb5c48c6c78a26b98b9
Anuj Upadhyay <anujupadhyay@Anujs-MacBook-Air.local>
2024-11-10 20:28:18 +0530

    fiexd some error

--- src/pages/AllPost.jsx
+++ src/pages/AllPost.jsx
@@ -1,8 +1,8 @@
 "use client";
 import React, { useState, useEffect } from "react";
 
-import { PostCard } from "../componenets/PostCard"; // Assuming typo fix for 'components'
-import { Container } from "../componenets/container/Container";
+import PostCard from "../componenets/PostCard.jsx"; // Assuming typo fix for 'components'
+import  Container  from "../componenets/container/Container";
 
 import service from "../appwrite/configAppwrite";
 import {
