afb0e974b227ac78ca78f04e52c5f13121f05c1d
Sawan <sawankushwah457@gmail.com>
2024-11-10 08:25:00 +0530

    Added blogger story page

--- src/main.jsx
+++ src/main.jsx
@@ -35,6 +35,7 @@ import Error404 from "./pages/Error404.jsx";
 import Features from "./pages/Features.jsx";
 import DiscussionForum from "./pages/DiscussionForum.jsx";
 import TermsOfUse from "./componenets/TermsOfUse.jsx";
+import Stories from "./pages/Stories.jsx";
 
 // Add this at the top of your existing index.js
 if ('serviceWorker' in navigator) {
@@ -146,6 +147,10 @@ const router = createBrowserRouter([
       {
         path: "/licensing",
         element: <LicensingPage />,
+      },
+      {
+        path: "/stories",
+        element: <Stories />,
       },
       {
         path: "/profile",
